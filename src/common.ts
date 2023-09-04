cc456d3373cda4764c6ea61f0dbe0c63860be8e6
Mike <mike.watling@nexusmods.com>
2023-09-04 19:12:47 +0100

    Update common.ts
    Added more common variables

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,53 @@
-import * as path from 'path';
-
 export const GAME_ID = 'starfield';
 export const XBOX_ID = 'BethesdaSoftworks.ProjectGold';
 export const STEAMAPP_ID = '1716740';
+
+export const DATA_SUBFOLDERS = [
+    'Meshes',
+    'Textures',
+    'FaceGen',
+    'Music',
+    'Sounds',
+    'MaxHeights',
+    'VIS',
+    'Grass',
+    'Strings',
+    'Materials',
+    'LODSettings',
+    'Misc',
+    'ShadersFX',
+    'PlanetData',
+    'Space',
+    'Terrian',
+    'GeoExporter',
+    'Noise',
+    'Particles',
+    'Geometries',
+    'Scripts',
+    'SFSE',
+    'LOD',
+    'Video',
+    'Interface'
+];
+
+export const TOP_LEVEL_COMPATIBILITY_FOLDERS = [
+    'root',
+    'Starfield',
+    'Starfield root'
+];
+
+export const DATA_EXTENSIONS = [
+    '.ba2', '.esm', '.esp', '.esl'
+];
+
+export const ROOT_EXTENSIONS = [
+    '.ini',
+    '.dll',
+    '.exe'
+];
+
+export const ROOT_FOLDERS = [
+    'Plugins', //ASI loader uses this... e.g. https://www.nexusmods.com/starfield/mods/252
+];
+
+export const SFSE_EXE = 'sfse_loader.exe';
